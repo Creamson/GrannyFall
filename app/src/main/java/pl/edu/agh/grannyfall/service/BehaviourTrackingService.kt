c4000b8fca7934b4f36a8019a45deb6351dbbedd
Jacek Stobiecki <jacek.stob@gmail.com>
2019-06-11 10:22:16 +0200

    Clean up project

--- app/src/main/java/pl/edu/agh/grannyfall/service/BehaviourTrackingService.kt
+++ app/src/main/java/pl/edu/agh/grannyfall/service/BehaviourTrackingService.kt
@@ -100,41 +100,14 @@ class BehaviourTrackingService : Service() {
 
     private fun generateBigTextStyleNotification(): Notification? {
 
-        // 1. Create/Retrieve Notification Channel for O and beyond devices (26+).
         val notificationChannelId = createNotificationChannel(this)
 
-
-        // 2. Build the BIG_TEXT_STYLE.
         val bigTextStyle = NotificationCompat.BigTextStyle()
-            // Overrides ContentText in the big form of the template.
             .bigText("Behaviour Service")
-            // Overrides ContentTitle in the big form of the template.
             .setBigContentTitle("Granny Fall")
 
-        // 3. Set up main Intent for notification.
         val notifyIntent = Intent(this, MainActivity::class.java)
 
-        // When creating your Intent, you need to take into account the back state, i.e., what
-        // happens after your Activity launches and the user presses the back button.
-
-        // There are two options:
-        //      1. Regular activity - You're starting an Activity that's part of the application's
-        //      normal workflow.
-
-        //      2. Special activity - The user only sees this Activity if it's started from a
-        //      notification. In a sense, the Activity extends the notification by providing
-        //      information that would be hard to display in the notification itself.
-
-        // For the BIG_TEXT_STYLE notification, we will consider the activity launched by the main
-        // Intent as a special activity, so we will follow option 2.
-
-        // For an example of option 1, check either the MESSAGING_STYLE or BIG_PICTURE_STYLE
-        // examples.
-
-        // For more information, check out our dev article:
-        // https://developer.android.com/training/notify-user/navigation.html
-
-        // Sets the Activity to start in a new, empty task
         notifyIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
 
         val notifyPendingIntent = PendingIntent.getActivity(
@@ -144,46 +117,21 @@ class BehaviourTrackingService : Service() {
             PendingIntent.FLAG_UPDATE_CURRENT
         )
 
-        // 5. Build and issue the notification.
-
-        // Because we want this to be a new notification (not updating a previous notification), we
-        // create a new Builder. Later, we use the same global builder to get back the notification
-        // we built here for the snooze action, that is, canceling the notification and relaunching
-        // it several seconds later.
-
-        // Notification Channel Id is ignored for Android pre O (26).
         val notificationCompatBuilder = NotificationCompat.Builder(
             applicationContext, notificationChannelId ?: ""
         )
 
         return notificationCompatBuilder
-            // BIG_TEXT_STYLE sets title and content for API 16 (4.1 and after).
             .setStyle(bigTextStyle)
-            // Content for API <24 (7.0 and below) devices.
             .setContentText("Behaviour Service Content Text")
             .setContentIntent(notifyPendingIntent)
             .setDefaults(NotificationCompat.DEFAULT_ALL)
-            // Set primary color (important for Wear 2.0 Notifications).
             .setColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
-
-            // SIDE NOTE: Auto-bundling is enabled for 4 or more notifications on API 24+ (N+)
-            // devices and all Wear devices. If you have more than one notification and
-            // you prefer a different summary notification, set a group key and create a
-            // summary notification via
-            // .setGroupSummary(true)
-            // .setGroup(GROUP_KEY_YOUR_NAME_HERE)
-
             .setCategory(Notification.CATEGORY_REMINDER)
-
-            // Sets priority for 25 and below. For 26 and above, 'priority' is deprecated for
-            // 'importance' which is set in the NotificationChannel. The integers representing
-            // 'priority' are different from 'importance', so make sure you don't mix them.
             .setPriority(Notification.PRIORITY_DEFAULT)
-
             .setVisibility(Notification.VISIBILITY_PRIVATE)
             .build()
     }
-
 
     companion object {
         private const val LOCK_NAME: String = "grannyfall:lockname"
@@ -198,7 +146,6 @@ fun createNotificationChannel(
     context: Context
 ): String? {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
         val channelId = "grannyfall:behaviourTrackingNotification"
 
         val notificationChannel = NotificationChannel(
@@ -216,7 +163,6 @@ fun createNotificationChannel(
 
         return channelId
     } else {
-        // Returns null for pre-O (26) devices.
         return null
     }
 }
